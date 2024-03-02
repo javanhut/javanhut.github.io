5cbcc04142196e8adda1a2fed9eb5da65e556858
Javan H. <javanstorm@gmail.com>
2024-03-02 03:20:11 -0500

    Changed the ' to escape chars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     </div>
     <h2>Education</h2>
     <div>
-    <p>I have a Bachelor's degree in Electrical Engineering from the Kennesaw State University, Marietta, Georgia.</p>
-    <p>I am currently pursuing a Master's degree in Computer Science with a concentration on Artifical Intelligence from the Kennesaw State University, Marietta, Georgia.</p>
+    <p>I have a Bachelor&apos;s degree in Electrical Engineering from the Kennesaw State University, Marietta, Georgia.</p>
+    <p>I am currently pursuing a Master&apso;s degree in Computer Science with a concentration on Artifical Intelligence from the Kennesaw State University, Marietta, Georgia.</p>
     </div>
     <h2>Skills</h2>
     <p>I am skilled in the following:</p>
