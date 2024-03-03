b3c40649800c3f4c3a8a7984d061228c9fe40cb4
Javan H. <javanstorm@gmail.com>
2024-03-03 15:59:38 -0500

    Added images for fun.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import MovieClip from '@/components/MovieClip';
+import Image from 'next/image';
+import why from '@/images/why.png';
+import mybrain from '@/images/mybrain.png';
 
 export default function Home() {
   return (
@@ -12,7 +15,7 @@ export default function Home() {
         <button className="btn btn-outline-success" type="submit">Search</button>
     </form>
         </div>
-        </nav><p>Welcome to the jungle ladies and gentlemen.</p>
+        </nav><p>This portfolio page is supposed to be professional....I am an engineer who enjoys what I do and this reflects my personality but I am professional when the need arises.(*Warning: Contains humor throughout)</p>
        <div className='accordian' id="accordianInfo">
         <div className='accordian-item'>
           <h2 className='accordian-header'>
@@ -20,6 +23,10 @@ export default function Home() {
           </h2>
           <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionInfo">
             <div className='accordian-body'>
+              <div className='fun-images'>
+              <Image src={why} alt="Me at work" width={200} height={200} /> 
+              <Image src={mybrain} alt="Problem solving for me pretty much" width={200} height={200} />
+              </div>
             <p><i>Personal Philosophy:</i> If I don&apos;t already know what it is I can learn it, If I can&apos;t learn it myself find someone who knows it and learn it from them. </p>
             <p>I consider myself a Electrical Software Engineer. As is I i feel like both a Electrical Engineer and Software Engineer. I love technology and all things science.</p>
             </div>
@@ -61,6 +68,7 @@ export default function Home() {
     <div className="list-group" id="list-tab" role="tablist">
       <a className="list-group-item list-group-item-action active" id="list-python-list" data-bs-toggle="list" href="#list-python" role="tab" aria-controls="list-python">Python</a>
       <a className="list-group-item list-group-item-action" id="list-java-list" data-bs-toggle="list" href="#list-java" role="tab" aria-controls="list-java">Java</a>
+      <a className="list-group-item list-group-item-action" id="list-kotlin-list" data-bs-toggle="list" href="#list-kotlin" role="tab" aria-controls="list-kotlin">Kotlin</a>
       <a className="list-group-item list-group-item-action" id="list-c-list" data-bs-toggle="list" href="#list-c" role="tab" aria-controls="list-c">C</a>
       <a className="list-group-item list-group-item-action" id="list-cpp-list" data-bs-toggle="list" href="#list-cpp" role="tab" aria-controls="list-cpp">C++</a>
       <a className="list-group-item list-group-item-action" id="list-javascript-list" data-bs-toggle="list" href="#list-javascript" role="tab" aria-controls="list-javascript">JavaScript</a>
@@ -73,6 +81,7 @@ export default function Home() {
     <div className="tab-content" id="nav-tabContent">
       <div className="tab-pane fade show active" id="list-python" role="tabpanel" aria-labelledby="list-python-list"><div><p>This is by far the language I&apos;m most proficient and have been coding in the longest.</p> <p>I have coded professionally in this language for about 2 years and contributed to mostly internal repositories with some Open Source contributions.</p> <p>Most of my coding projects are in this language. And the third coding language I learned. </p></div></div>
       <div className="tab-pane fade" id="list-java" role="tabpanel" aria-labelledby="list-java-list"><div><p>This is the language I abhor the most. Not very intuitive and my least favorite language I&apos;ve used, Used it for university assigments and that&apos;s pretty much it.</p> <p>I know it&apos;s supposed to be used for so much, I can code in it I just don&apos;t enjoy it</p></div> </div>
+      <div className="tab-pane fade" id="list-kotlin" role="tabpanel" aria-labelledby="list-kotlin-list"><div><p>I learned kotlin while trying to build an app for Android and I desperately wanted to avoid Java with a passion. Fun alternative to Java.</p></div></div>
       <div className="tab-pane fade" id="list-c" role="tabpanel" aria-labelledby="list-c-list"><div><p>Mother of most modern languages, C is fantastic. I enjoy it although going from the OOP nature of Python, Java, C++ was alittle difficult at first.</p><p>I&apos;m saddened that i haven&apos;t had more chances to code in this language so i&apos;ll make more projects in this language maybe some linux device drivers or a language</p></div></div>
       <div className="tab-pane fade" id="list-cpp" role="tabpanel" aria-labelledby="list-cpp-list"><div><p>Ahh C++ way too powerful for it&apos;s own good I&apos;m not expert in this language but I will try to be it&apos;s just that good.</p><p>You&apos;ll likely see a few projects from me in this language</p></div></div>
       <div className="tab-pane fade" id="list-javascript" role="tabpanel" aria-labelledby="list-javascript-list"><div><p>Well I made this whole page in react using javascript and typescript. That&apos;s it that&apos;s my stance.</p></div></div>
